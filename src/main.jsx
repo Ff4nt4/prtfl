037c95244fdf18caa316ce6b6226c6d59a58ae90
CM <Clopac80@gmail.com>
2026-08-17 20:11:01 +0200

    Add BrowserRouter to main.jsx
    Wrap the App component with BrowserRouter for routing.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' // AGGIUNGI QUESTO
 import App from './App.jsx'
-import './index.css' // O il tuo file CSS principale
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
