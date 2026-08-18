13f470e8bb15774dab802179a8d7c5133938047f
CM <Clopac80@gmail.com>
2026-08-18 11:56:55 +0200

    Update educational and exhibition information

--- src/ContactsPage.jsx
+++ src/ContactsPage.jsx
@@ -44,35 +44,26 @@ const sectionHeadingStyle = {
 const ABOUT_TEXT = `Claudia Mangone (b.1996)
 Lives and works between Bergamo and Milan
 
-Education
+EDUCATION
 2021 - 2023 Master in Visual Art, ÉCAL, Lausanne
 2016 - 2021 Bachelor  in Visual Art,  Accademia di Belle Arti di Brera, Milan
 
-Solo Exhibitions
+SOLO EXHIBITIONS
 2026 Culvert, riss(e), Varese
 2026 Collyrium, Bacheca, Firenze
 2023 Black’n’yellow Black’n’yellow, AnonimaKunsthalle, Varese
 
-Selected Group Exhibitions
-
-2026
-- Magnolia, San Carpoforo, Milan
-
+SELECTED GROUP EXHIBITIONS
+2026 Magnolia, San Carpoforo, Milan
 2025
 - Ventaglio, Clima Gallery, Milan
 - Mostrina, Palazzo Bronzo, Genoa
 - Incandescenze e maree, YAG Garage, Pescara
+2024 I Re non toccano le porte, Casa Scaglioni, Castelponzone
+2022 Midsummer Night’s Dream, Notgalerie Skulpturenallee Seestadt, Vienna
+2021 Pick Clique, Como Contemporanea, Como
 
-2024
-- I Re non toccano le porte, Casa Scaglioni, Castelponzone
-
-2022
-- Midsummer Night’s Dream, Notgalerie Skulpturenallee Seestadt, Vienna
-
-2021
-- Pick Clique, Como Contemporanea, Como
-
-Residencies
+RESIDENCIES
 2026 ongoing Supertoscana at Centro Pecci and MudaC, Prato, Carrara
 2025 Talents! Nuovo Grand Tour at Fondation Fiminco, Paris
 2025 Culture Moves Europe with Fils Sous Filature, Paris
